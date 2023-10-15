cf67a60d983a824d40bf82d94ac3f816fb9ed35f
Luka23785 <115667413+Luka23785@users.noreply.github.com>
2023-10-16 00:01:15 +0200

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+typedef struct {
+	char ime[10];
+	char prezime[10];
+	double bodovi;
+}student;
+
+int main() {
+	int brojredova = 0;
+	char charbuffer[1024] = "";
+	FILE* filepointer = NULL;
+	filepointer = fopen("tekst.txt", "r");
+	if (filepointer == NULL) {
+		printf("datoteka nije pronadjena!\n");
+		return 1;
+
+	}
+	while (foef(filepointer)) {
+		if (fgets(charbuffer, 1024, filepointer) == "\n")
+			brojredova++;
+	}
+	printf("%d", brojredova);
+	fclose(filepointer);
+	return 0;
+}
